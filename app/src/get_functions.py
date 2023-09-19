f32aad992e66238fdaeb507e6c43726c314f7623
desilascarvalho <62862972+desilascarvalho@users.noreply.github.com>
2023-09-19 13:23:32 +0100

    Update get_functions.py

--- app/src/get_functions.py
+++ app/src/get_functions.py
@@ -1,12 +1,25 @@
 import subprocess
 import time
 import logging
+import datetime
 
 def run_command(command):
     subprocess.run(command, shell=True)
 
-def run_refresh_command(date):
-    command = f"deemon refresh --time-machine {date.strftime('%Y-%m-%d')}"
+def run_refresh_command():
+    # Obtém a data atual
+    today = datetime.date.today()
+
+    # Calcula a data do dia anterior
+    yesterday = today - datetime.timedelta(days=1)
+
+    # Formata a data do dia anterior no formato 'YYYY-MM-DD'
+    yesterday_formatted = yesterday.strftime('%Y-%m-%d')
+
+    # Constrói o comando com a data do dia anterior
+    command = f"deemon refresh --time-machine {yesterday_formatted}"
+
+    # Executa o comando
     run_command(command)
 
 def run_import_command():
